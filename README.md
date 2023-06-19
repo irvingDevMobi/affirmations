cdde23d5413d476b8c917d356b3a8a8cc426c817
Irving Lop <irving.dev.mobi@gmail.com>
2023-06-19 14:00:07 -0600

    U3 -> P2 -> 2 Add a scrollable list

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Project of the Basid Android with Compose course, section
 Unit 3 Displays Lists and Use Material Design
 Pathway 2 Build a Scrollable list
+
+<img src="screenshots/scrollList.gif" />
--- app/src/main/java/dev/irving/affirmations/MainActivity.kt
+++ app/src/main/java/dev/irving/affirmations/MainActivity.kt
@@ -3,44 +3,92 @@ package dev.irving.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.irving.affirmations.data.Datasource
+import dev.irving.affirmations.model.Affirmation
 import dev.irving.affirmations.ui.theme.AffirmationsTheme
+import dev.irving.affirmations.ui.theme.Purple80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AffirmationsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            AffirmationApp()
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun AffirmationApp() {
+    AffirmationsTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AffirmationList(Datasource.loadAffirmations())
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AffirmationList(
+    affirmations: List<Affirmation>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(affirmations) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AffirmationsTheme {
-        Greeting("Android")
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.background(Purple80, shape = RoundedCornerShape(8.dp)),
+        ) {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop,
+            )
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
     }
 }
